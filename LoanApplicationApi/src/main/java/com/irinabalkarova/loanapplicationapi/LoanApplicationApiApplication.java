da1de0c43adac6ad7a025a25b94f47bcb1b02842
IrinaBBB <balkarovairina@gmail.com>
2023-04-29 19:09:20 +0200

    add: initial setup for LoanApplicationApi

---
+++ LoanApplicationApi/src/main/java/com/irinabalkarova/loanapplicationapi/LoanApplicationApiApplication.java
@@ -0,0 +1,13 @@
+package com.irinabalkarova.loanapplicationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplicationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApplicationApiApplication.class, args);
+    }
+
+}
---
+++ LoanApplicationApi/src/test/java/com/irinabalkarova/loanapplicationapi/LoanApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irinabalkarova.loanapplicationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
