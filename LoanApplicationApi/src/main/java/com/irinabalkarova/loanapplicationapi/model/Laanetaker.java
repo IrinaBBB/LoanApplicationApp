7493db54338835e987957eb43bf6d0103b466abb
Irina <balkarovairina@gmail.com>
2023-04-29 21:39:19 +0200

    add: Laanetaker entity

---
+++ LoanApplicationApi/src/main/java/com/irinabalkarova/loanapplicationapi/model/Laanetaker.java
@@ -0,0 +1,27 @@
+package com.irinabalkarova.loanapplicationapi.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class Laanetaker {
+    private Long fodselsnummer;
+    private String navn;
+    private LocalDate fodselsdato;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Laanetaker)) return false;
+
+        Laanetaker that = (Laanetaker) o;
+
+        return getFodselsnummer().equals(that.getFodselsnummer());
+    }
+
+    @Override
+    public int hashCode() {
+        return getFodselsnummer().hashCode();
+    }
+}
