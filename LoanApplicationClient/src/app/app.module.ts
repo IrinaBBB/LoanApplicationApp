784409cadcda33d3fd393e34f2bc3f3183a6b8b5
Irina <balkarovairina@gmail.com>
2023-05-01 11:54:24 +0200

    add: Angular routing

--- LoanApplicationClient/src/app/app.module.ts
+++ LoanApplicationClient/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoanApplicationComponent } from './loan-application/loan-application.component';
+import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [{ path: '', component: LoanApplicationComponent }];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, LoanApplicationComponent],
+    imports: [BrowserModule, RouterModule.forRoot(routes), HttpClientModule],
+    exports: [RouterModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
