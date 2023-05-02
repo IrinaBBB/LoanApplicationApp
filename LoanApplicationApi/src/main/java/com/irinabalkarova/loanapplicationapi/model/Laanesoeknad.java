da59bd2b2ea33ce71299bdc8d0a6c9722508326d
Irina <balkarovairina@gmail.com>
2023-05-02 09:01:51 +0200

    add: Lombok and methods equals()/hashCode() to Laanesoeknad

--- LoanApplicationApi/src/main/java/com/irinabalkarova/loanapplicationapi/model/Laanesoeknad.java
+++ LoanApplicationApi/src/main/java/com/irinabalkarova/loanapplicationapi/model/Laanesoeknad.java
@@ -1,7 +1,10 @@
 package com.irinabalkarova.loanapplicationapi.model;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class Laanesoeknad {
     private long id;
     private List<Laanetaker> lanetakere;
@@ -11,4 +14,19 @@ public class Laanesoeknad {
     private int avdragsfriPeriode;
     private Laanetype type;
     private Status status;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Laanesoeknad)) return false;
+
+        Laanesoeknad that = (Laanesoeknad) o;
+
+        return getLanetakere() != null ? getLanetakere().equals(that.getLanetakere()) : that.getLanetakere() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getLanetakere() != null ? getLanetakere().hashCode() : 0;
+    }
 }
