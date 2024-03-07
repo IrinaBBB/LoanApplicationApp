4badc64163cae9d512650a68e4a3deaaa7f0e1fc
IrinaB <balkarovairina@gmail.com>
2024-03-07 08:26:52 +0100

    add: LoanAppOpenApi

---
+++ LoanAppOpenApi/src/main/java/ru/aurorahost/LoanAppOpenApi/LoanAppOpenApiApplication.java
@@ -0,0 +1,13 @@
+package ru.aurorahost.LoanAppOpenApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanAppOpenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanAppOpenApiApplication.class, args);
+	}
+
+}
---
+++ LoanAppOpenApi/src/test/java/ru/aurorahost/LoanAppOpenApi/LoanAppOpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aurorahost.LoanAppOpenApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanAppOpenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
